5c1e314830b7111fa634d49ab494cfa69e238a9c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 05:04:04 +0530

    Till Lecture 21

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
     this.loginService.sendCredential(this.credential.username, this.credential.password).subscribe(
       res => {
         console.log(res);
-        localStorage.set("xAuthToken", res.json().token);
+        localStorage.setItem("xAuthToken", res.json().token);
         this.loggedIn = true;
         location.reload();
       },
