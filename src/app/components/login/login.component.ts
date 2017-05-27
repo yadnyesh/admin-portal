27216f4c0094f08c78761c67af0e0f6c340d0a32
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 04:21:06 +0530

    Test

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private credential = {'username': '', 'password': ''};
+  private loggedIn = false;
+
   constructor() { }
 
   ngOnInit() {
