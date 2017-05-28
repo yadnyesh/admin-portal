dce3b29183c7215318d01f013bff9413e7b47d4b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 15:24:02 +0530

    Update login service

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,6 +17,14 @@ export class NavBarComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.loginService.checkSession().subscribe(
+      res => {
+        this.loggedIn=true;
+      },
+      error => {
+        this.loggedIn=false;
+      }
+    );
   }
 
 }
