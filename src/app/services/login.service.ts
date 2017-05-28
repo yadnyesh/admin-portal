d7584f59547df9e4f44acac70e631c8fd6b4a73e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 14:53:35 +0530

    Update login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -21,9 +21,7 @@ export class LoginService {
   }
 
   checkSession() {
-    let url = "http://localhost:8181/token";
-    let encodedCredentials = btoa(username + ":" + password);
-    let basicHeader = "Basic " + encodedCredentials;
+    let url = "http://localhost:8181/checkSession";
     let headers = new Headers ({
       'Content-Type': 'application/x-www-form-urlencoded',
       'Authorization' : basicHeader
