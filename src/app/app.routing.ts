aa2521f378345f5dbd2c6052168eb05deafcc895
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 12:00:26 +0530

    Update app routing ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,9 @@
 import {ModuleWithProviders}          from '@angular/core';
-import {Routes, RouteModule}          from '@angular/core';
+import {Routes, RouterModule}          from '@angular/router';
 import {LoginComponent}               from './components/login/login.component';
 
 const appRoutes: Routes = [
-  
+
 ];
+
+export const routing: ModuleWithProviders = RouteModule.forRoot(appRoutes)
