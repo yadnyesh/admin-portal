128d60b7ffdca7e8b98e5d29195b5d96f2f5fcfd
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 15:29:52 +0530

    Update login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -29,5 +29,13 @@ export class LoginService {
     return this.http.get(url,{headers: headers});
   }
 
+  checkSession() {
+    let url = "http://localhost:8181/checkSession";
+    let headers = new Headers ({
+      'x-auth-token' : localStorage.getItem('xAuthToken')
+    });
+
+    return this.http.get(url,{headers: headers});
+  }
 
 }
