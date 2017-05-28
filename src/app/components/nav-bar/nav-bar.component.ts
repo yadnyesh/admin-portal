a8b6a585682d632422216684305c68e70c19ec03
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 19:04:02 +0530

    Tweaks

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
