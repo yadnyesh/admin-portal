3bd869d38fd54ebe145d4fcba4c4c7be4afeec51
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 19:01:37 +0530

    Tweaks

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import {ModuleWithProviders}          from '@angular/core';
-import {Routes, RouterModule}          from '@angular/router';
+import {Routes, RouterModule}         from '@angular/router';
 import {LoginComponent}               from './components/login/login.component';
+import {AddNewBookComponent}          from './components/add-new-book/add-new-book.component'; 
 
 const appRoutes: Routes = [
   {
