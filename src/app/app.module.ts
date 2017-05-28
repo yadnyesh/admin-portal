7cbb88706d0737d78899280471f54ba05f329d4b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 11:50:36 +0530

    Modfied app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { FormsModule }            from '@angular/forms';
 import { HttpModule }             from '@angular/http';
 import { routing }                from './app.routing';
 import {MaterialModule}           from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule}  from '@angular/platform-browser/animations';
 import 'hammerjs';
 
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { LoginComponent } from './components/login/login.component';
+import { AppComponent }           from './app.component';
+import { NavBarComponent }        from './components/nav-bar/nav-bar.component';
+import { LoginComponent }         from './components/login/login.component';
 
-import { LoginService } from './services/login.service';
+import { LoginService }           from './services/login.service';
 
 @NgModule({
   declarations: [
